e1032033c13523cb751f7ed3e80cde3cbd1e5fc2
Jannes <jannes.venter@allegiance.co.za>
2021-01-27 09:11:35 +0200

    V1.0 Alpha access
    stores items to local storage
    known bugs:
    checkbox state not saved to local storage

---
+++ NeedDoinList/NeedDoinList.Models/ListItem.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace NeedDoinList.Models
+{
+    public class ListItem
+    {
+        public bool Checked { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ NeedDoinList/NeedDoinList/Models/ListItem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace NeedDoinList.Models
+{
+    public class ListItem
+    {
+        private bool boxchecked;
+
+        public bool BoxChecked
+        {
+            get { return boxchecked; }
+            set { boxchecked = value; }
+        }
+
+        //[Required]
+        //[StringLength(50, ErrorMessage = "Character limit reached(50 character limit).")]
+        private string text;
+
+        public string Text
+        {
+            get { return text; }
+            set { text = value; }
+        }
+
+        public ListItem(bool boxchecked, string text)
+        {
+            this.boxchecked = boxchecked;
+            this.text = text;
+        }
+    }
+}
